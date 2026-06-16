1af5af355388554e18e557b4da929304515324b7
Bhuaal <ayushsinghbca2022@gmail.com>
2026-06-16 22:58:15 +0530

    Adding-auth.js

--- src/app.js
+++ src/app.js
@@ -2,13 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello jee");
+const { adminAuth, userAuth } = require("./middlewares/Auth");
+
+app.use("/admin", adminAuth);
+
+app.get("/admin", (req, res) => {
+  res.send("All data sent");
 });
 
-app.use("/path",(req,res) => {
-    res.send("this route is path ")
-})
+app.get("/user", userAuth, (req, res) => {
+  res.send("User data sent");
+});
+
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("All data deleted");
+});
 
 app.listen(3000, () => {
   console.log("Server started");
